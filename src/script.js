e7a3f51d97af2b2229a9fe6cbbeae35e32cb2480
Sebastian Wachter <sebastian.wachter@online.de>
2020-07-10 12:53:29 +0200

    remove gcloud dependencies + update packages + improve readability + resolve breaking changes

--- src/script.js
+++ src/script.js
@@ -10,6 +10,7 @@ import {
   Points,
   WebGLRenderer,
 } from 'three';
+
 import okIMG from './assets/ok.png';
 import cryIMG from './assets/cry.png';
 import hundredIMG from './assets/100.png';
@@ -41,28 +42,28 @@ const resize = () => {
   windowSize.y = window.innerHeight;
   windowSize.halfX = window.innerWidth / 2;
   windowSize.halfY = window.innerHeight / 2;
+
   camera.aspect = windowSize.x / windowSize.y;
   camera.updateProjectionMatrix();
+
   renderer.setSize(windowSize.x, windowSize.y);
 };
 
-const mouseMove = (event) => {
-  mousePos.mouseX = event.clientX - windowSize.halfX;
-  mousePos.mouseY = event.clientY - windowSize.halfY;
+const mouseMove = ({ clientX, clientY }) => {
+  mousePos.mouseX = clientX - windowSize.halfX;
+  mousePos.mouseY = clientY - windowSize.halfY;
 };
 
 const touchStart = (event) => {
-  if (event.touches.length === 1) {
-    mousePos.mouseX = event.touches[0].pageX - windowSize.halfX;
-    mousePos.mouseY = event.touches[0].pageY - windowSize.halfY;
-  }
+  if (event.touches.length !== 1) return;
+  mousePos.mouseX = event.touches[0].pageX - windowSize.halfX;
+  mousePos.mouseY = event.touches[0].pageY - windowSize.halfY;
 };
 
 const touchMove = (event) => {
-  if (event.touches.length === 1) {
-    mousePos.mouseX = event.touches[0].pageX - windowSize.halfX;
-    mousePos.mouseY = event.touches[0].pageY - windowSize.halfY;
-  }
+  if (event.touches.length !== 1) return;
+  mousePos.mouseX = event.touches[0].pageX - windowSize.halfX;
+  mousePos.mouseY = event.touches[0].pageY - windowSize.halfY;
 };
 
 const init = () => {
@@ -70,6 +71,7 @@ const init = () => {
   camera.position.z = 1000;
   scene = new Scene();
   scene.fog = new FogExp2(0x000000, 0.0008);
+
   const geometry = new BufferGeometry();
   const vertices = [];
   const textureLoader = new TextureLoader();
@@ -77,16 +79,20 @@ const init = () => {
   const cry = textureLoader.load(cryIMG);
   const hundred = textureLoader.load(hundredIMG);
   const fire = textureLoader.load(fireIMG);
+
   for (let i = 0; i < 800; i += 1) {
     vertices.push(randomPos(), randomPos(), randomPos());
   }
-  geometry.addAttribute('position', new Float32BufferAttribute(vertices, 3));
+
+  geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
+
   parameters = [
     { sprite: ok, size: 20 },
     { sprite: cry, size: 15 },
     { sprite: hundred, size: 25 },
     { sprite: fire, size: 10 },
   ];
+
   parameters.forEach(({ sprite, size }, index) => {
     materials[index] = new PointsMaterial({
       size,
@@ -95,34 +101,45 @@ const init = () => {
       depthTest: false,
       transparent: true,
     });
+
     const particles = new Points(geometry, materials[index]);
+
     particles.rotation.x = randomParticleRot();
     particles.rotation.y = randomParticleRot();
     particles.rotation.z = randomParticleRot();
+
     scene.add(particles);
   });
+
   renderer = new WebGLRenderer({ alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(windowSize.x, windowSize.y);
   document.querySelector('main').appendChild(renderer.domElement);
+
   document.addEventListener('mousemove', mouseMove, false);
   document.addEventListener('touchstart', touchStart, false);
   document.addEventListener('touchmove', touchMove, false);
+
   window.addEventListener('resize', resize, false);
 };
 
 const animate = () => {
   requestAnimationFrame(animate);
+
   const time = Date.now() * 0.00005;
+
   camera.position.x += (mousePos.mouseX - camera.position.x) * 0.05;
   camera.position.y += (-mousePos.mouseY - camera.position.y) * 0.05;
   camera.lookAt(scene.position);
+
   for (let i = 0; i < scene.children.length; i += 1) {
     const object = scene.children[i];
+
     if (object instanceof Points) {
       object.rotation.z = time * (i < 5 ? i + 1 : -(i + 1));
     }
   }
+
   renderer.render(scene, camera);
 };
 
